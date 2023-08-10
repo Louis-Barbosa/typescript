b72013e633ef971a98bf9e5fa17974ea04d8abe2
Louis-Barbosa <barbosalouis07@gmail.com>
2023-08-10 18:33:05 -0400

    All bugs fixed

--- challenge3.js
+++ challenge3.js
@@ -1,22 +1,3 @@
-export {};
-
-interface Car {
-    model: string;
-    manufacturer: string;
-    year: number;
-    color: string;
-    inWarranty: boolean
-    miles?: number;
-}
-
-const corolla: Car = {
-    model: "Corolla",
-    manufacturer: "Toyota",
-    year: 2006,
-    color: "Red",
-    inWarranty: true
-}
-
 interface Country{
     continent: string;
     language: string;
